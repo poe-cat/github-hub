bd68301b8e87cf125741645c4c30dd10f6f8f341
poe-cat <katarzyna.tramea@gmail.com>
2023-06-04 16:11:34 +0200

    created method that uses GitHub API token (connect to the GitHub API using the github-api library)

--- src/main/java/com/poecat/githubhub/controller/UserController.java
+++ src/main/java/com/poecat/githubhub/controller/UserController.java
@@ -1,38 +1,56 @@
 package com.poecat.githubhub.controller;
 
+import com.poecat.githubhub.info.BranchInfo;
 import com.poecat.githubhub.info.RepositoryInfo;
-import org.springframework.http.HttpHeaders;
+import org.kohsuke.github.GHBranch;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-@Controller
-@RequestMapping("/users")
+@RestController
+@RequestMapping("/api")
 public class UserController {
 
-    private static final String GITHUB_API_URL = "https://api.github.com";
+    private static final String GITHUB_API_TOKEN = "YOUR_GITHUB_API_KEY";
 
-    @GetMapping("/{username}/repositories")
-    @ResponseBody
-    public ResponseEntity<List<RepositoryInfo>> getRepositories(@PathVariable String username) {
+    @GetMapping(value = "/repositories/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getRepositories(@PathVariable String username,
+                                             @RequestHeader(value = "Accept") String acceptHeader) {
 
-        // GET repositories response
-        String repositoriesUrl = GITHUB_API_URL + "/users/" + username + "/repos?type=all&fork=false";
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+        try {
+            GitHub github = GitHub.connectUsingOAuth(GITHUB_API_TOKEN);
+            GHRepository[] repositories = github.getUser(username).getRepositories().values().toArray(new GHRepository[0]);
 
-        ResponseEntity<List> repositoriesResponse = restTemplate.getForEntity(repositoriesUrl, List.class);
+            // not include forked repositories
+            List<RepositoryInfo> result = new ArrayList<>();
+            for (GHRepository repository : repositories) {
+                if (!repository.isFork()) {
+                    String repoName = repository.getName();
+                    String ownerLogin = repository.getOwnerName();
+
+                    List<BranchInfo> branches = new ArrayList<>();
+                    for (GHBranch branch : repository.getBranches().values()) {
+                        String branchName = branch.getName();
+                        String lastCommitSha = branch.getSHA1();
+                        branches.add(new BranchInfo(branchName, lastCommitSha));
+                    }
+
+                    result.add(new RepositoryInfo(repoName, ownerLogin, branches));
+                }
+            }
+
+            return ResponseEntity.ok(result);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
 
-        return null;
     }
-
 }
