400e9b16497149dc137d0acf2ecc26951f5735e7
poe-cat <katarzyna.tramea@gmail.com>
2023-06-04 10:27:38 +0200

    make GET request to list repositories

--- src/main/java/com/poecat/githubhub/controller/UserController.java
+++ src/main/java/com/poecat/githubhub/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.poecat.githubhub.controller;
 
 import com.poecat.githubhub.info.RepositoryInfo;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -19,6 +22,15 @@ public class UserController {
     @GetMapping("/{username}/repositories")
     @ResponseBody
     public ResponseEntity<List<RepositoryInfo>> getRepositories(@PathVariable String username) {
+
+        // GET repositories response
+        String repositoriesUrl = GITHUB_API_URL + "/users/" + username + "/repos?type=all&fork=false";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+
+        ResponseEntity<List> repositoriesResponse = restTemplate.getForEntity(repositoriesUrl, List.class);
+
 
         return null;
     }
